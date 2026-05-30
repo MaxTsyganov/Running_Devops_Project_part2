f473acb04441d9e13783e49cdb01fe8586bcb67d
Your Name <your.email@example.com>
2026-05-30 20:19:58 +0300

    chore: purge ignored files and enforce gitignore rules

--- ansible/run_ansible.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/expect -f
-spawn ansible-playbook -i inventory.ini playbook.yml --private-key ""C:UsersmaxmaOneDriveDesktopPython-projectsRunning ProjectRunning Project p2Running_Devops_Project_part2devops-project-key.pem"" --ask-vault-pass
-expect "Vault password:"
-send "11223311\r"
-expect eof
